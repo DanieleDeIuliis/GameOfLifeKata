c4a78d0023c4f4bd1f9d4636c4f987f6e3fa6310
Daniele De Iuliis <daniele.deiuliis@gmail.com>
2021-07-05 14:13:39 +0200

    extracted count on top alive neighbours

--- src/main/kotlin/com/dan/gameoflife/GameOfLife.kt
+++ src/main/kotlin/com/dan/gameoflife/GameOfLife.kt
@@ -3,11 +3,17 @@ package com.dan.gameoflife
 class GameOfLife(private val board: List<MutableList<Boolean>>) {
     fun nextGeneration() {
         board[0][1] = false
+        if(countOnTopAliveNeighbours(1,1) >= 2) {
+            board[1][1] = true
+        }
+    }
+
+    private fun countOnTopAliveNeighbours(row: Int, column: Int): Int {
         var count = 0
-        if(board[0][0]) count++
-        if(board[0][1]) count++
-        if(board[0][2]) count++
-        if(count >= 2) board[1][1] = true
+        if (board[row - 1][column - 1]) count++
+        if (board[row - 1][column]) count++
+        if (board[row - 1][column + 1]) count++
+        return count
     }
 
 }
