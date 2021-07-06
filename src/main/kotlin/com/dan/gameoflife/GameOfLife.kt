fa102c5714e007c1fe3aac509e7d94795e3abfb0
Daniele De Iuliis <daniele.deiuliis@gmail.com>
2021-07-06 15:27:06 +0200

    refactor

--- src/main/kotlin/com/dan/gameoflife/GameOfLife.kt
+++ src/main/kotlin/com/dan/gameoflife/GameOfLife.kt
@@ -1,22 +1,34 @@
 package com.dan.gameoflife
 
 class GameOfLife(private val board: List<MutableList<Boolean>>) {
+
     fun nextGeneration() {
-        val temp = mutableListOf<MutableList<Boolean>>()
-        board.forEachIndexed { rowIndex, row ->
-            temp.add(mutableListOf())
-            row.forEachIndexed { columnIndex, _ ->
-                temp[rowIndex].add(countOnTopAliveNeighbours(rowIndex,columnIndex) +
-                        countOnLineAliveNeighbours(rowIndex,columnIndex) +
-                        countBottomAliveNeighbours(rowIndex,columnIndex) >= 2)
-            }
-        }
-        temp.forEachIndexed { rowIndex, row ->
+        computeNextGeneration().copyTo(board)
+    }
+
+    private fun List<List<Boolean>>.copyTo(destination: List<MutableList<Boolean>>) {
+        this.forEachIndexed { rowIndex, row ->
             row.forEachIndexed { columnIndex, cell ->
-                board[rowIndex][columnIndex] = cell
+                destination[rowIndex][columnIndex] = cell
             }
         }
     }
+
+    private fun computeNextGeneration(): List<List<Boolean>> {
+        val next = mutableListOf<MutableList<Boolean>>()
+        board.forEachIndexed { rowIndex, row ->
+            next.add(mutableListOf())
+            row.forEachIndexed { columnIndex, _ ->
+                next[rowIndex].add(isSurvived(rowIndex, columnIndex))
+            }
+        }
+        return next
+    }
+
+    private fun isSurvived(rowIndex: Int, columnIndex: Int) =
+        countOnTopAliveNeighbours(rowIndex, columnIndex) +
+                countOnLineAliveNeighbours(rowIndex, columnIndex) +
+                countBottomAliveNeighbours(rowIndex, columnIndex) >= 2
 
     private fun countOnLineAliveNeighbours(row: Int, column: Int): Int {
         var count = 0
